760d5c7d5aa5437b20c1df79b8066876a75dadea
Matias Baez <mbcorp.matias@gmail.com>
2024-07-06 20:50:46 +0000

    Se nego el estado del botón para que el LD1 este apagado si se presiona B1_USER y se agrego comentarios

--- main.cpp
+++ main.cpp
@@ -2,11 +2,15 @@
 
 int main()
 {
+    // Especifica como entrada el pin en la constante BUTTON1
+    // B1_USER esta ligado al pin fisico
     DigitalIn B1_USER(BUTTON1);
 
+    // Especifica como salida el pin en la constante LED1
+    // LD1 esta ligado al pin fisico
     DigitalOut LD1(LED1);
 
     while (true) {
-        LD1 = B1_USER;
+        LD1 = !B1_USER;
     }
 }
